36edbc7b668833176c3d9538d85428b2b7f6073e
Benji Thompson <thompson.benji@gmail.com>
2021-06-02 17:27:38 -0400

    post route successfully adds to local mongo db

--- controllers/communities.js
+++ controllers/communities.js
@@ -10,6 +10,14 @@ router.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+// CREATE COMMUNITY ROUTE
+router.post('/', (req, res) => {
+    Community.create(req.body, (error, createdCommunity) => {
+        res.redirect('/');
+    });
+});
+
+// GET NEW COMMUNITY ROUTE
 router.get('/new', (req, res) => {
     res.render('new.ejs');
 });
--- models/communities.js
+++ models/communities.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const communitySchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: String,
-    admin: [ { type: String, required: true } ],
-    member: [ String ]
 }, { timestamps: true });
 
 const Community = mongoose.model('Community', communitySchema);
